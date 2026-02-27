72bc48a59ddd2ec82f2728b0da98199fb7a65846
Ganesh Walse <ganeshwalse1975@gmail.com>
2026-02-28 00:26:47 +0530

    top rated restraunt feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import RestrauntCard from "./RestrauntCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRest, setlistOfRest] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filteredRest = listOfRest.filter(
+            (rest) => rest.info.avgRating > 4.2,
+          );
+          setlistOfRest(filteredRest);
+        }}
+      >
+        Top Rated Restraunt
+      </button>
 
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRest.map((restaurant) => (
           <RestrauntCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
